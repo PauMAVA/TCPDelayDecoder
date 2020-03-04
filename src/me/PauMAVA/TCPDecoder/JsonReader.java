c580d22ce09aa4ac0e10a08d2226e3600c18d2d3
PauMAVA <paumachetti@gmail.com>
2020-03-04 22:27:27 +0100

    Initial commit

---
+++ src/me/PauMAVA/TCPDecoder/JsonReader.java
@@ -0,0 +1,40 @@
+package me.PauMAVA.TCPDecoder;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JsonReader {
+
+    private File file;
+
+    public JsonReader(File file) {
+        this.file = file;
+    }
+
+    public HashMap<Integer, Double> getDelayFromPackets() {
+        try {
+            String data = new String(Files.readAllBytes(this.file.toPath()));
+            String regexFrameTime = Pattern.quote("\"tcp.time_delta\"") + "(.*?)" + Pattern.quote("\r");
+            String regexFrameNum = Pattern.quote("\"frame.number\"") + "(.*?)" + Pattern.quote(",");
+            Pattern frameTimePattern = Pattern.compile(regexFrameTime);
+            Pattern frameNumPattern = Pattern.compile(regexFrameNum);
+            Matcher frameTimeMatcher = frameTimePattern.matcher(data);
+            Matcher frameNumMatcher = frameNumPattern.matcher(data);
+            HashMap<Integer, Double> map = new HashMap<>();
+            while (frameNumMatcher.find() && frameTimeMatcher.find()) {
+                Integer i = Integer.parseInt(frameNumMatcher.group(1).replace(" ", "").replace("\t", "").replace("\n", "").replace(":", "").replace("\"", "").replace(",", ""));
+                Double d = Double.parseDouble(frameTimeMatcher.group(1).replace(" ", "").replace("\t", "").replace("\n", "").replace(":", "").replace("\"", "").replace(",", ""));
+                map.put(i, d);
+            }
+            return map;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new HashMap<Integer, Double>();
+        }
+    }
+
+}
---
+++ src/me/PauMAVA/TCPDecoder/TDDCore.java
@@ -0,0 +1,53 @@
+package me.PauMAVA.TCPDecoder;
+
+import java.io.File;
+import java.util.*;
+
+public class TDDCore {
+
+    private JsonReader reader;
+    private int counter = 0;
+
+    public static void main(String[] args) {
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            System.out.println("The file " + args[0] + " was not found!");
+        } else {
+            new TDDCore().execute(file);
+        }
+    }
+
+    public void execute(File file) {
+        this.reader = new JsonReader(file);
+        System.out.println(this.reader.getDelayFromPackets().toString());
+        extractBinaryData(this.reader.getDelayFromPackets());
+    }
+
+    private void extractBinaryData(HashMap<Integer, Double> map) {
+        List<Integer> list = new ArrayList<>();
+        List<Double> values = new ArrayList<>(map.values());
+        for (int i = 3; i < values.size(); i +=2) {
+            if (values.get(i) < 0.1D) {
+                list.add(0);
+            } else {
+                list.add(1);
+            }
+        }
+        list.stream().forEach(this::print);
+        Collections.reverse(list);
+        System.out.println("");
+        counter = 0;
+        list.stream().forEach(this::print);
+    }
+
+    private void print(int i) {
+        System.out.print(i);
+        if (counter == 7) {
+            counter = 0;
+            System.out.print(" ");
+        } else {
+            counter++;
+        }
+    }
+
+}
